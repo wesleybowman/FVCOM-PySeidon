bc2cad606f3d4cdfa6847facd7e740f6b6d9a2c8
wesleybowman <wesley.bowman23@gmail.com>
2014-07-14 14:59:05 -0300

    added comments in fvcomClass about harmonics and reconstr

--- fvcomClass/fvcomClass.py
+++ fvcomClass/fvcomClass.py
@@ -85,7 +85,53 @@ Notes:
 
 
     def harmonics(self, ind, twodim=True, **kwarg):
-        '''Use/Inputs/Outputs of this method has to be clarified !!!'''
+        '''
+        Description:
+        ----------
+        harmonics calls ut_solv. Depending on whether the user wants velocity
+        or elevation, it will call the correct version of ut_solv based on the
+        twodim option.
+
+        Inputs:
+        ------
+        Takes an index so that ut_solv can be run on the data and twodim.
+        twodim=True means that ut_solv will be done for velocity, and
+        twodim=False means that ut_solv will be done for elevation.
+
+        Options:
+        -------
+        Options are the same as for ut_solv, which are shown below with
+        their default values:
+            conf_int=True
+            cnstit='auto'
+            notrend=0
+            prefilt=[]
+            nodsatlint=0
+            nodsatnone=0
+            gwchlint=0
+            gwchnone=0
+            infer=[]
+            inferaprx=0
+            rmin=1
+            method='cauchy'
+            tunrdn=1
+            linci=0
+            white=0
+            nrlzn=200
+            lsfrqosmp=1
+            nodiagn=0
+            diagnplots=0
+            diagnminsnr=2
+            ordercnstit=[]
+            runtimedisp='yyy'
+
+        Notes:
+        -----
+        For more detailed information about ut_solv, please see
+        https://github.com/wesleybowman/UTide
+
+        '''
+
         #TR_comments: Add debug flag in Utide: debug=self._debug
         if twodim:
             self.coef = ut_solv(self.Variables.matlabTime, self.Variables.ua[:, ind],
@@ -99,33 +145,66 @@ Notes:
             self.QC.append('ut_solv done for elevation')
 
     def reconstr(self, time):
-        '''Use/Inputs/Outputs of this method has to be clarified !!!'''
+        '''
+        Description:
+        ----------
+        reconstr calls ut_reconstr. This function assumes harmonics (ut_solv)
+        has already been executed. If it has not, it will inform the user of
+        the error and ask them to run harmonics. It asks the user to run it
+        since it needs an index at which to run, and there isn't a default
+        index.
+
+        Inputs:
+        ------
+        Takes a time series for ut_reconstr to do the reconstruction to.
+
+        Options:
+        -------
+        Options are the same as for ut_reconstr, which are shown below with
+        their default values:
+            cnstit = []
+            minsnr = 2
+            minpe = 0
+
+        Notes:
+        -----
+        For more detailed information about ut_reconstr, please see
+        https://github.com/wesleybowman/UTide
+
+        '''
+
         #TR_comments: Add debug flag in Utide: debug=self._debug
-        if self.coef['aux']['opt']['twodim']:
-            self.U, self.V = ut_reconstr(time, self.coef)
-            self.QC.append('ut_reconstr done for velocity')
+        if hasattr(self, 'coef'):
+            if self.coef['aux']['opt']['twodim']:
+                self.U, self.V = ut_reconstr(time, self.coef)
+                self.QC.append('ut_reconstr done for velocity')
+            else:
+                self.ts_recon, _ = ut_reconstr(time, self.coef)
+                self.QC.append('ut_reconstr done for elevation')
+
         else:
-            self.ts_recon, _ = ut_reconstr(time, self.coef)
-            self.QC.append('ut_reconstr done for elevation')
+            print 'Error: Please run harmonics before reconstr'
+
+
 
 #Test section when running in shell >> python fvcomClass.py
 if __name__ == '__main__':
 
     filename = './test_file/dn_coarse_0001.nc'
-    #filename = '/home/wesley/ncfiles/smallcape_force_0001.nc'
+    filename = '/home/wesley/ncfiles/smallcape_force_0001.nc'
     test = FVCOM(filename)
-    test.harmonics(0, cnstit='auto', notrend=True, nodiagn=True)
+    #test.harmonics(0, cnstit='auto', notrend=True, nodiagn=True)
     #WB_COMMENTS: fixed matlabttime to matlabtime
     test.reconstr(test.Variables.matlabTime)
 
     #WB_COMMENTS: This doesn't work with your TR variable convention
     # t = shortest_element_path(test.latc,test.lonc,test.lat,test.lon,test.nv,test.h)
-    t = shortest_element_path(test.Variables.latc, test.Variables.lonc,
-                              test.Variables.lat, test.Variables.lon,
-                              test.Variables.nv, test.Variables.h)
-
-    elements, _ = t.getTargets([[41420, 39763], [48484, 53441],
-                                [27241, 24226], [21706, 17458]])
+#    t = shortest_element_path(test.Variables.latc, test.Variables.lonc,
+#                              test.Variables.lat, test.Variables.lon,
+#                              test.Variables.nv, test.Variables.h)
+#
+#    elements, _ = t.getTargets([[41420, 39763], [48484, 53441],
+#                                [27241, 24226], [21706, 17458]])
 
 
 
