8a25df3fc2762be25f72fc557af197d668250ee0
wesleybowman <wesley.bowman23@gmail.com>
2014-07-14 15:00:59 -0300

    fixed misspelled license

--- __init__.py
+++ __init__.py
@@ -15,6 +15,5 @@ from adcpClass import *
 
 __version__ = '0.0'
 __authors__ = ['Thomas Roc, Wesley Bowman, Jonathan Smith']
-__licence__ = 'python site du zero __init__.py template'
+__license__ = 'python site du zero __init__.py template'
 __copyright__ = 'Copyright (c) 2014 EcoEnergyII'
-
